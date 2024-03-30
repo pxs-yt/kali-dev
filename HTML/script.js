0197da8f42a41a6acb45d0895eb5d937acbda964
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-03-30 10:22:58 +0000

    modified script.js again

--- HTML/script.js
+++ HTML/script.js
@@ -17,3 +17,60 @@ if (navigator.getBattery) {
 } else {
     console.log("Battery Status API is not supported on this browser.");
 }
+
+// Print additional navigator properties
+console.log("appCodeName: " + navigator.appCodeName);
+console.log("appName: " + navigator.appName);
+console.log("appVersion: " + navigator.appVersion);
+console.log("cookieEnabled: " + navigator.cookieEnabled);
+console.log("language: " + navigator.language);
+console.log("onLine: " + navigator.onLine);
+console.log("platform: " + navigator.platform);
+console.log("product: " + navigator.product);
+console.log("javaEnabled: " + navigator.javaEnabled());
+
+// Print experimental navigator properties
+if (navigator.bluetooth) {
+    console.log("bluetooth: " + navigator.bluetooth);
+} else {
+    console.log("Bluetooth API is not supported on this browser.");
+}
+
+if (navigator.clipboard) {
+    console.log("clipboard: " + navigator.clipboard);
+} else {
+    console.log("Clipboard API is not supported on this browser.");
+}
+
+if (navigator.connection) {
+    console.log("connection: " + navigator.connection);
+} else {
+    console.log("Network Information API is not supported on this browser.");
+}
+
+if (navigator.deviceMemory) {
+    console.log("deviceMemory: " + navigator.deviceMemory);
+} else {
+    console.log("Device Memory API is not supported on this browser.");
+}
+
+if (navigator.gpu) {
+    console.log("gpu: " + navigator.gpu);
+} else {
+    console.log("WebGPU API is not supported on this browser.");
+}
+
+if (navigator.hardwareConcurrency) {
+    console.log("hardwareConcurrency: " + navigator.hardwareConcurrency);
+} else {
+    console.log("Hardware Concurrency API is not supported on this browser.");
+}
+
+// Print geolocation
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        console.log("Geolocation: " + position.coords.latitude + ", " + position.coords.longitude);
+    });
+} else {
+    console.log("Geolocation is not supported by this browser.");
+}
