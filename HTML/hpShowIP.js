00e30b651f46840f5a06feac8e6336251d9c787a
pxs-yt <pxsyt.24@gmail.com>
2024-03-31 23:27:05 +0200

    added function that shows user ip

---
+++ HTML/hpShowIP.js
@@ -0,0 +1,8 @@
+fetch('https://api.ipify.org?format=json')
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('ip').textContent = data.ip;
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
