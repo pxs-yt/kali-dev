99428e1f78e79f9f31be5f4a67619676f35b370e
pxs-yt <pxsyt.24@gmail.com>
2024-04-01 14:38:07 +0200

    added a simple snake game

--- HTML/buttonFunction.js
+++ HTML/buttonFunction.js
@@ -1,3 +1,7 @@
 document.getElementById("404").onclick = function () {
     location.href = "https://pxs-yt.github.io/kali-dev/HTML/404.html"
 }
+
+document.getElementById("snakegame").onclick = function () {
+    location.href = "https://pxs-yt.github.io/kali-dev/snakegame/index.html"
+}
