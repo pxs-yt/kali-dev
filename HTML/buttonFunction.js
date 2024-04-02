1adff36753a1597a053fe6c8fa5ab80800ccc84c
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-04-02 18:51:11 +0200

    small change

--- HTML/buttonFunction.js
+++ HTML/buttonFunction.js
@@ -5,3 +5,7 @@ document.getElementById("404").onclick = function () {
 document.getElementById("snakegame").onclick = function () {
     location.href = "https://pxs-yt.github.io/kali-dev/snakegame/index.html"
 }
+
+document.getElementById("adsterra-direct-link-btn").onclick = function () {
+    location.href = "https://tremblesyringeillusive.com/qfbwvi7t?key=084fbdac3156b65e1c2454c5f81beba1"
+}
