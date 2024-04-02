4540aca3a3edeba821edce8014a8d5ef990dc3ba
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-04-02 18:59:12 +0200

    .

--- HTML/buttonFunction.js
+++ HTML/buttonFunction.js
@@ -9,3 +9,7 @@ document.getElementById("snakegame").onclick = function () {
 document.getElementById("adsterra-direct-link-btn").onclick = function () {
     location.href = "https://tremblesyringeillusive.com/qfbwvi7t?key=084fbdac3156b65e1c2454c5f81beba1"
 }
+
+document.getElementById("fdm-file-1").onclick = function () {
+    location.href = "HTML/fdm-file/The Mission (2023) [2160p] [WEBRip] [x265] [10bit] [5.1] [YTS.MX].torrent"
+}
