adedf65977a11e1d1402f15a6d232166e6ff7709
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-04-03 14:06:37 +0200

    ads-js added

---
+++ ads-js/ads.js
@@ -0,0 +1,11 @@
+// Popunder
+document.write('<script type="text/javascript" src="//tremblesyringeillusive.com/70/ba/53/70ba53420ab8d572accccf7f64de329b.js"></script>');
+
+// Direct Link
+document.write('<a href="https://tremblesyringeillusive.com/qfbwvi7t?key=084fbdac3156b65e1c2454c5f81beba1>DL</a>');
+
+// Native Banners
+document.write('<script async="async" data-cfasync="false" src="//tremblesyringeillusive.com/2e4a88352859e2facb5d6915f2133df2/invoke.js"></script>');
+
+// Static Banners
+document.write('<script type="text/javascript" src="//tremblesyringeillusive.com/fd1127d67e1177b2b8ebd626f876f578/invoke.js"></script>');
