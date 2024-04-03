1ed0f94d59b25a42a33dda1a038933be8b491906
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-04-03 20:17:42 +0200

    small change

--- HTML/buttonFunction.js
+++ HTML/buttonFunction.js
@@ -10,6 +10,6 @@ document.getElementById("adsterra-direct-link-btn").onclick = function () {
     location.href = "https://tremblesyringeillusive.com/qfbwvi7t?key=084fbdac3156b65e1c2454c5f81beba1"
 }
 
-document.getElementById("fdm-file-1").onclick = function () {
-    location.href = "https://pxs-yt.github.io/kali-dev/HTML/fdm-file/fdm-file-1.1.torrent"
+document.getElementById("urls-pxs").onclick = function () {
+    location.href = "https://cloaking.link/URLs-PXS"
 }
