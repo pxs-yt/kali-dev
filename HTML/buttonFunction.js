76fdc37a89e3dd980aa48e846def8fc67a43d7e1
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-04-08 18:34:41 +0200

    /

--- HTML/buttonFunction.js
+++ HTML/buttonFunction.js
@@ -14,6 +14,6 @@ document.getElementById("urls-pxs").onclick = function () {
     location.href = "https://cloaking.link/URLs-PXS"
 }
 
-document.getElementById("earnapp-dw").onclick = function () {
-    location.href = "https://pxs-yt.github.io/kali-dev/HTML/earnapp-1.409.981.apk"
+document.getElementById("pxsapp-dw").onclick = function () {
+    location.href = "https://apk.e-droid.net/apk/app3020927-2bt4mf.apk?v=5"
 }
